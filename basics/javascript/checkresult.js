9fa521eb9efd76d6c1cd36202bc2c705b92a550e
realincor <saurabhrathee95@gmail.com>
2026-03-30 23:28:12 +0530

    30-03-2026

---
+++ basics/javascript/checkresult.js
@@ -0,0 +1,5 @@
+let name  = "Saurabh";
+console.log(name);
+
+let name2 = `Saurabh`;
+console.log(name2);
---
+++ basics/javascript/practice.js
@@ -0,0 +1,6 @@
+let name = "John";
+let age = 25;
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+let city = "New York";
+console.log(`I live in ${city}.`);
