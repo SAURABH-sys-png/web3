680750092e403b24a65f576ae9c8ca5be0a84cba
realincor <saurabhrathee95@gmail.com>
2026-04-07 17:25:42 +0000

    07-04-2026

---
+++ learning_projects/Defence_Elgibilty_Calc/calculator.js
@@ -0,0 +1,9 @@
+
+
+function check() {
+    let dob = document.getElementById("dob").value;
+    let stream = document.getElementById("stream").value;
+
+    console.log(dob);
+    console.log(stream);
+}
